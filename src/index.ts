079204adb77fad5343163052c31bc447a9e4cf02
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2022-02-17 14:56:25 +0000

    Remove newline

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,3 @@ export type { AtomicStoreState, SetAtomPayload } from './atomic-redux-state/atom
 export type { Atom, WritableAtom } from './atomic-redux-state/atom-types';
 export { derivedAtom } from './atomic-redux-state/derived-atom';
 export type { AtomUpdateCallback, DefaultValue, LoadingAtom } from './atomic-redux-state/getter-setter-utils';
-
