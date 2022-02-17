099f5384a3a36fa6aba501a8a89f5d699d7b1653
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2022-02-17 17:10:37 +0000

    Update exported values from index

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ export { atom } from './atomic-redux-state/atom';
 export { getAtomMiddleware } from './atomic-redux-state/atom-middleware';
 export {
     default as atomsReducer,
+    getAtomValueFromState,
     getAtomValueFromStore,
     isAtomUpdating,
     setAtom
 } from './atomic-redux-state/atom-slice';
 export type { AtomicStoreState, SetAtomPayload } from './atomic-redux-state/atom-slice';
-export type { Atom, WritableAtom } from './atomic-redux-state/atom-types';
+export type { Atom, SyncOrAsyncValue, WritableAtom } from './atomic-redux-state/atom-types';
 export { derivedAtom } from './atomic-redux-state/derived-atom';
-export type { AtomUpdateCallback, DefaultValue, LoadingAtom } from './atomic-redux-state/getter-setter-utils';
+export { DefaultValue, LoadingAtom } from './atomic-redux-state/getter-setter-utils';
+export type {
+    AsyncAtomValue, AtomUpdateCallback, AtomValue, ValueOrSetter
+} from './atomic-redux-state/getter-setter-utils';
